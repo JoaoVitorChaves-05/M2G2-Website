c9e22eed3db0e1fc56980be542797170c52136ed
João Vitor Mancio Chaves <joaovmanciochaves@gmail.com>
2022-02-20 15:48:10 -0300

    update

--- index.js
+++ index.js
@@ -1,10 +1,33 @@
 const express = require('express')
 const app = express()
+require('dotenv').config()
 
 app.use(express.static('public'))
+app.use(express.urlencoded({extended: false}))
+app.use(express.json());
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/public/views/index.html')
+})
+
+app.get('/admin', (req, res) => {
+    const {auth} = req.query
+    
+    if (!auth.length || auth != process.env.AUTH_TOKEN) {
+        res.send('<h1>Acesso não autorizado</h1>')
+    }
+
+    res.sendFile(__dirname + '/public/views/admin.html')
+})
+
+app.post('/login', (req, res) => {
+    const {login, password} = req.body
+
+    console.log(process.env.AUTH_TOKEN)
+    if (login == 'admin' && password == 'admin')
+        res.redirect('/admin?auth=' + process.env.AUTH_TOKEN)
+    else
+        res.send('<h1>Acesso não autorizado</h1>')
 })
 
 app.listen(process.env.PORT || 3000), () => {
---
+++ public/scripts/form.js
@@ -0,0 +1,35 @@
+const options = {
+    'residencial': [
+        'Apartamento',
+        'Casa',
+        'Casa em condomínio',
+        'Chácara',
+        'Cobertura',
+        'Flat',
+        'Kitnet',
+        'Lote',
+        'Sobrado',
+        'Fazenda/Sítio/Chácara'
+    ],
+    'comercial': [
+        'Consultório',
+        'Galpão/Depósito/Armazém',
+        'Imóvel comercial',
+        'Lote/Terreno',
+        'Ponto comercial/loja/box',
+        'Sala/Conjunto',
+        'Prédio/Ed. Inteiro'
+    ]
+}
+
+function setForm() {
+    const categoriaImovel = document.querySelector('#categoriaImovel').value
+
+    const tipoImovel = document.querySelector('#tipoImovel').children[0]
+
+    if (categoriaImovel != null) {
+        options[categoriaImovel].forEach(e => {
+            tipoImovel.innerHTML += `<option value="">${e}</option>`
+        })
+    }
+}
